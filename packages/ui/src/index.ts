3452d4760ef81ac92f5c9e002843b47591cf5102
mxvsh <xencodes@icloud.com>
2025-07-26 11:56:59 +0530

    Add ui package

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,14 @@
+export * from "./components/ui/avatar"
+export * from "./components/ui/button"
+export * from "./components/ui/card"
+export * from "./components/ui/dialog"
+export * from "./components/ui/dropdown-menu"
+export * from "./components/ui/form"
+export * from "./components/ui/input"
+export * from "./components/ui/label"
+export * from "./components/ui/menubar"
+export * from "./components/ui/separator"
+export * from "./components/ui/sonner";
+export * from "./components/ui/tabs"
+
+export * from "./lib/utils"
