7db97695a9c8fed14c06a002dc5d5dbb574aae20
mxvsh <xencodes@icloud.com>
2025-08-20 13:11:43 +0530

    chore: add privacy page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import EditorPage from "@/pages/EditorPage";
 import HomePage from "@/pages/HomePage";
+import PrivacyPage from "@/pages/PrivacyPage";
 
 function App() {
 	return (
 		<Routes>
 			<Route index path="/" element={<HomePage />} />
 			<Route path="/space/:spaceId" element={<EditorPage />} />
+			<Route path="/privacy" element={<PrivacyPage />} />
 		</Routes>
 	);
 }
